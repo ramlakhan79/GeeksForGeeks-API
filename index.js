61a99673b82240bfb7cfb06ff67c6ac3b9d02311
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2024-09-12 02:02:33 +0530

    update path

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ const PORT = process.env.PORT || 4000;
 // Middleware for parsing JSON data
 app.use(express.json());
 
+app.use("/",(req, res)=>{
+    res.json({ message: "Please use like this format",
+        format:"https://geeks-for-geeks-api-five.vercel.app/api/user/Your_User_Name"
+     });
+})
+
 // Routes
-app.use("/", userRoutes);
+app.use("/api/user/", userRoutes);
 
 // Starting the server
 app.listen(PORT, () => {
