af8d594ad35e58b0e698ed8e27d6efc309f5d713
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2024-09-12 02:06:30 +0530

    Update

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 4000;
 // Middleware for parsing JSON data
 app.use(express.json());
 
-app.use("/",(req, res)=>{
-    res.json({ message: "Please use like this format",
-        format:"https://geeks-for-geeks-api-five.vercel.app/api/user/Your_User_Name"
-     });
-})
+// app.use("/",(req, res)=>{
+//     res.json({ message: "Please use like this format",
+//         format:"https://geeks-for-geeks-api-five.vercel.app/api/user/Your_User_Name"
+//      });
+// })
 
 // Routes
 app.use("/api/user/", userRoutes);
