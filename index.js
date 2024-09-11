a4999011fed8fd24e4b37c8d534520d7638152a4
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2024-09-12 01:53:35 +0530

    update path

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 // Routes
-app.use("/api/user", userRoutes);
+app.use("/", userRoutes);
 
 // Starting the server
 app.listen(PORT, () => {
